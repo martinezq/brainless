b620379970277c9ab1a72b4821f495292a230aa5
martinezq <marcin.koch.pl@gmail.com>
2013-03-22 09:26:46 +0100

    +hello world test

---
+++ brainless-all/src/test/java/org/mysoft/brainless/sandbox/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package org.mysoft.brainless.sandbox;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void hello_world() {
+		assertTrue(true);
+	}
+	
+}
