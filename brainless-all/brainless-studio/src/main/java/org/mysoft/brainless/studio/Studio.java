a054d21fb02fa27fbc3b8587bc2cfbdc278e9e03
martinezq <marcin.koch.pl@gmail.com>
2013-09-04 15:25:54 +0200

    +brainless-studio

---
+++ brainless-all/brainless-studio/src/main/java/org/mysoft/brainless/studio/Studio.java
@@ -0,0 +1,13 @@
+package org.mysoft.brainless.studio;
+
+public class Studio {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
